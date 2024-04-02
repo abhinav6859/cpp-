b2c68ab61fb5478f20f7b08bcee83f9d0e9a6fb3
abhinav6859 <abhinavvats06665@gmail.com>
2024-04-02 16:40:01 +0530

    initial commit

--- basic c++/char_dt.cpp
+++ basic c++/char_dt.cpp
@@ -16,7 +16,28 @@ int main(){
     
     char value = 65 ; // ASCII character code for 'A'
     cout << "value : " << value << endl; // A
-    cout << "value(int) : " << static_cast<int>(value) <<"\n"; 
+    cout << "value(int) : " <<(int)value<<"\n"; 
+ //static_cast<int>(value) it can also write with place of (int)value
 
-    return 0;
+//     char input;
+//     int asciiValue;
+
+//     cout << "Enter a character: ";
+//     cin >> input;
+
+//     // Printing ASCII value of the character
+//     asciiValue = input;
+//     cout << "ASCII value of " << input << " is: " << asciiValue << endl;
+
+//     // Converting ASCII value to character
+//     cout << "Enter an ASCII value: ";
+//     cin >> asciiValue;
+//     cout << "Character of ASCII value " << asciiValue << " is: " << char(asciiValue) << endl;
+
+//     return 0;
+// }
+
+    
+cout <<char(69);// integer to ascii value
+cout<<"\n";
 }
